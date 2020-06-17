66efd0027c6bbfd9eb5855fd4198e4dabe8e4556
victor <vayudin_1@edu.hse.ru>
2020-06-17 14:58:36 +0300

    add left-menu

--- dashboard/src/app/app.module.ts
+++ dashboard/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LeftMenuComponent } from './left-menu/left-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LeftMenuComponent
   ],
   imports: [
     BrowserModule,
